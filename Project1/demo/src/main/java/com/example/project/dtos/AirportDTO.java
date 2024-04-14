04877a1217f0a8deddb910d93bd031b0e278ed1a
Stily-TUES <stilianqo@gmail.com>
2024-04-14 15:12:05 +0300

    added DTOS and repos

---
+++ Project1/demo/src/main/java/com/example/project/dtos/AirportDTO.java
@@ -0,0 +1,20 @@
+package com.example.project.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AirportDTO {
+    private Long id;
+    private String name;
+    private CityDTO city;
+    private String iataCode;
+    private String icaoCode;
+    private Double latitude;
+    private Double longitude;
+}
---
+++ Project1/demo/src/main/java/com/example/project/repositories/CityRepository.java
@@ -0,0 +1,10 @@
+package com.example.project.repositories;
+
+import com.viki.baziproject1.models.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+    Optional<City> findByName(String name);
+}
